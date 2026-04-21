9a335d7d1d6397eb38dc83374a155e2479b3f952
masmoudi emna <emnam0468@gmail.com>
2026-04-21 03:39:09 +0100

    formateur

--- E-Learning-platform-main/elearning-frontend/src/app/shared/layout/educal-footer/educal-footer.ts
+++ E-Learning-platform-main/elearning-frontend/src/app/shared/layout/educal-footer/educal-footer.ts
@@ -1,14 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../services/auth';
-// OU chemin relatif depuis le fichier :
-// import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-educal-footer',
   templateUrl: './educal-footer.html',
   standalone: true,
-  imports: [RouterModule],  
+  imports: [CommonModule, RouterModule],
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
@@ -18,18 +17,75 @@ export class FooterComponent {
     private router: Router
   ) {}
 
+  get isAuthenticated(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
   naviguerConnexion(): void {
     if (!this.authService.isLoggedIn()) {
       this.router.navigate(['/login']);
       return;
     }
+
     const role = this.authService.getRole();
+
     if (role === 'ADMIN') {
       this.router.navigate(['/admin']);
     } else if (role === 'TEACHER') {
       this.router.navigate(['/teacher']);
     } else {
       this.router.navigate(['/student']);
+    }
+  }
+
+  changeSection(section: string): void {
+    const role = this.authService.getRole();
+
+    if (role === 'ADMIN') {
+      switch (section) {
+        case 'dashboard':
+          this.router.navigate(['/admin']);
+          break;
+        case 'catalogue':
+          this.router.navigate(['/home'], { fragment: 'courses' });
+          break;
+        case 'mesCours':
+          this.router.navigate(['/admin-cours']);
+          break;
+        case 'profil':
+          this.router.navigate(['/admin']);
+          break;
+      }
+    } else if (role === 'TEACHER') {
+      switch (section) {
+        case 'dashboard':
+          this.router.navigate(['/teacher']);
+          break;
+        case 'catalogue':
+          this.router.navigate(['/home'], { fragment: 'courses' });
+          break;
+        case 'mesCours':
+          this.router.navigate(['/teacher']);
+          break;
+        case 'profil':
+          this.router.navigate(['/teacher-profile']);
+          break;
+      }
+    } else {
+      switch (section) {
+        case 'dashboard':
+          this.router.navigate(['/student']);
+          break;
+        case 'catalogue':
+          this.router.navigate(['/home'], { fragment: 'courses' });
+          break;
+        case 'mesCours':
+          this.router.navigate(['/student']);
+          break;
+        case 'profil':
+          this.router.navigate(['/student']);
+          break;
+      }
     }
   }
 }
